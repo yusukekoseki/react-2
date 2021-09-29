308e5229a1fbb47c3787545010a77366c6ed7fd1
yusukekoseki <88299973+yusukekoseki@users.noreply.github.com>
2021-09-29 11:31:55 +0900

    Update footer.js
    特に変更なしです

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,6 +4,9 @@ const Footer = () => {
   return (
     <div className="footer">
       <span className="copylight">© 2021 koseki Corp.</span>
+      <small>
+        <a href="https://newsapi.org">Powered by NewsAPI.org</a>
+      </small>
       <a className="contact" href="/contact/" target="_blank">
         お問い合わせ
       </a>
